301b769e9404afedd6b4932f60297c8e3d383d3c
맥동우 <t6u80o@naver.com>
2024-12-23 00:59:16 +0900

    localstorge를 활용한 좋아요 기능 구현

--- app/@util/hooks/useLikeButtonHandler.tsx
+++ app/@util/hooks/useLikeButtonHandler.tsx
@@ -1,22 +1,41 @@
-'use client';
+"use client";
 // type
 import { ObjectId } from "mongodb";
 // fetch func
 import patchLike from "../function/fetch/patchLike";
 // react
 import { useState } from "react";
+// localstorage
+import {
+  checkLikeAlreadyExist,
+  updateLocalStorge,
+} from "../function/general/LocalStorage/likes";
 
-export default function useLikeButtonHandler(like : number){
+export default function useLikeButtonHandler(like: number) {
   const [likeState, setLikeState] = useState(like);
 
-  async function likeButtonHandler (id : ObjectId){
-    try{
-      await patchLike(id);
-    }catch(error){
+  async function likeButtonHandler(id: ObjectId) {
+    try {
+      const updateSuccess = await handleLikeState(id);
+      const likeCount = updateSuccess ? 1 : 0;
+      setLikeState((pre) => pre + likeCount);
+    } catch (error) {
       throw error;
     }
-    setLikeState((pre) => pre + 1);
   }
 
   return { likeState, likeButtonHandler };
+}
+
+async function handleLikeState(id: ObjectId) {
+  const isAlreadyHeartClick = checkLikeAlreadyExist(id);
+
+  if (!isAlreadyHeartClick) {
+    await patchLike(id);
+    updateLocalStorge(id);
+    return true;
+  }
+
+  alert("이미 좋아요를 누른 레시피입니다.");
+  return false;
 }
--- app/components/FoodCard/CardName.tsx
+++ app/components/FoodCard/CardName.tsx
@@ -1,6 +1,6 @@
 export default function CardName({ name }: { name: string }) {
   return (
-    <div className="flex-[3_3_0%]">
+    <div className="flex-[3_3_0%] truncate">
       {name}
     </div>
   );
